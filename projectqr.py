54fa806c5669c77c8b264b9ed0e71fecfbe174f8
SriCharan-23 <sricharanrao451@gmail.com>
2026-04-05 17:53:31 +0530

    initial commit

---
+++ projectqr.py
@@ -0,0 +1,16 @@
+import qrcode
+
+
+url = input("Enter any URL:").strip()
+file_path = "qrcode.png"
+
+
+qr = qrcode.QRCode()
+qr.add_data(url)
+
+
+img = qr.make_image()
+img.save(file_path)
+
+
+print("QR Code Generated Successfully!")
