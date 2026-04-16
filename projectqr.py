7a4a585ecb252bf8e005dda72c60def052195505
HIBARE SRI CHARAN RAO <sricharanrao451@gmail.com>
2026-04-16 23:30:08 +0530

    Update projectqr.py

--- projectqr.py
+++ projectqr.py
@@ -1,11 +1,11 @@
-import qrcode
+import qrcode  # inbuilt module in python
 
 
-url = input("Enter any URL:").strip()
-file_path = "qrcode.png"
+url = input("Enter any URL:").strip() # Taking input
+file_path = "qrcode.png"   # path selection
 
 
-qr = qrcode.QRCode()
+qr = qrcode.QRCode() # object creation
 qr.add_data(url)
 
 
